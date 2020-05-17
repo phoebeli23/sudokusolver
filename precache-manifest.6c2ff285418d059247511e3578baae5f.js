348a32196fac5986c727e57a1dd8c4a6e633a524
Phoebe Li <phoebeli@mbp19.attlocal.net>
2020-05-17 16:28:11 -0700

    Updates

---
+++ precache-manifest.6c2ff285418d059247511e3578baae5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31bf6afcc5492ea634855c36399df7bc",
+    "url": "/sudokusolver/index.html"
+  },
+  {
+    "revision": "7208ad6e2d5ed1ddee40",
+    "url": "/sudokusolver/static/css/main.710de097.chunk.css"
+  },
+  {
+    "revision": "a4ab2ac9fd3fc4497971",
+    "url": "/sudokusolver/static/js/2.f060c519.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sudokusolver/static/js/2.f060c519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7208ad6e2d5ed1ddee40",
+    "url": "/sudokusolver/static/js/main.57ed9603.chunk.js"
+  },
+  {
+    "revision": "f02b0fa6d22b20d0fb3a",
+    "url": "/sudokusolver/static/js/runtime-main.635cf955.js"
+  }
+]);
